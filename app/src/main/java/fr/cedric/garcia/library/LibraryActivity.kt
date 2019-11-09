bc2d8dc2669724f8519e89b01d9504b0af738979
CedricG44 <cedric-garcia@sfr.fr>
2019-11-09 09:26:28 +0100

    feat: add Arrow

--- app/src/main/java/fr/cedric/garcia/library/LibraryActivity.kt
+++ app/src/main/java/fr/cedric/garcia/library/LibraryActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import fr.cedric.garcia.library.book.Book
 import fr.cedric.garcia.library.book.BookAdapter
 import fr.cedric.garcia.library.repositories.HenriPotierRepository
 import fr.cedric.garcia.library.services.HenriPotierService
@@ -21,18 +22,31 @@ class LibraryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listRecyclerView = findViewById<RecyclerView>(R.id.bookListView)
-        listRecyclerView.layoutManager = LinearLayoutManager(this)
+        val bookListRecyclerView = findViewById<RecyclerView>(R.id.bookListView)
+        bookListRecyclerView.layoutManager = LinearLayoutManager(this)
+        createBookList(bookListRecyclerView)
+    }
 
-
+    private fun createBookList(bookList: RecyclerView) =
         CoroutineScope(Dispatchers.Main).launch {
             val books = booksRepository.getBooks()
+            var bookAdapter = BookAdapter(this@LibraryActivity, emptyList())
+
             withContext(Dispatchers.IO) {
-                books.forEach {
-                    Log.d("book", it.toString())
-                }
+                bookAdapter = BookAdapter(
+                    this@LibraryActivity,
+                    books.fold({
+                        Log.e("getBooks", it.message, it)
+                        emptyList<Book>()
+                    }, {
+                        it.forEach { book ->
+                            Log.d("book", book.toString())
+                        }
+                        it
+                    })
+                )
             }
-            listRecyclerView.adapter = BookAdapter(this@LibraryActivity, books)
+
+            bookList.adapter = bookAdapter
         }
-    }
 }
--- app/src/main/java/fr/cedric/garcia/library/repositories/HenriPotierRepository.kt
+++ app/src/main/java/fr/cedric/garcia/library/repositories/HenriPotierRepository.kt
@@ -1,11 +1,16 @@
 package fr.cedric.garcia.library.repositories
 
+import arrow.core.Either
 import fr.cedric.garcia.library.book.Book
 import fr.cedric.garcia.library.services.HenriPotierService
 
 class HenriPotierRepository(private val service: HenriPotierService) {
 
-    suspend fun getBooks(): List<Book> {
-        return service.getBooks().await()
+    suspend fun getBooks(): Either<Throwable, List<Book>> {
+        return try {
+            Either.right(service.getBooks().await())
+        } catch (ex: Exception) {
+            Either.left(ex)
+        }
     }
 }
