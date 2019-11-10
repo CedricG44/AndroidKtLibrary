5f0afe7bc5cae843f5c88ea0afb11e4970a7655d
CedricG44 <cedric-garcia@sfr.fr>
2019-11-10 13:00:05 +0100

    feat: book cover

--- app/src/main/java/fr/cedric/garcia/library/book/BookItemView.kt
+++ app/src/main/java/fr/cedric/garcia/library/book/BookItemView.kt
@@ -2,8 +2,10 @@ package fr.cedric.garcia.library.book
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import com.squareup.picasso.Picasso
 import fr.cedric.garcia.library.R
 
 class BookItemView @JvmOverloads constructor(
@@ -14,15 +16,19 @@ class BookItemView @JvmOverloads constructor(
 
     private lateinit var titleView: TextView
     private lateinit var priceView: TextView
+    private lateinit var coverImageView: ImageView
 
     override fun onFinishInflate() {
         super.onFinishInflate()
         titleView = findViewById(R.id.titleTextView)
         priceView = findViewById(R.id.priceTextView)
+        coverImageView = findViewById(R.id.coverImageView)
     }
 
     fun bindView(book: Book) {
         titleView.text = book.title
         priceView.text = book.price
+
+        Picasso.get().load(book.cover).into(coverImageView)
     }
 }
